b72e9ebf2a6caac9c6efa02202f7f281a949688f
Aditya Sarawat <54805791+aditya-sarawat@users.noreply.github.com>
2020-12-01 22:34:16 +0530

    Version 1.0

--- password_generator.py
+++ password_generator.py
@@ -8,29 +8,11 @@ PUNCTUATION = string.punctuation
 
 
 def get_password_length():
-    """
-    Retrieves the length of a password
-    :returns number <class 'int'>
-    """
     length_of_password = input("How long do you want your password: ")
     return int(length_of_password)
 
 
 def password_generator(cbl, password_length=8):
-    """
-    Generates a random password having the specified length
-    :length -> length of password to be generated. Defaults to 8
-        if nothing is specified
-    :cbl-> a list of boolean values representing a user choice for
-        string constant to be used to generate password.
-        0 item ---> digits
-             True to add digits to constant False otherwise
-        1 item ---> letters
-             True to add letters to constant False otherwise
-        2 item ---> punctuation
-             True to add punctuation to constant False otherwise
-    :returns string <class 'str'>
-    """
     # create alphanumerical by fetching string constant
     printable = fetch_string_constant(cbl)
 
@@ -47,16 +29,6 @@ def password_generator(cbl, password_length=8):
 
 
 def password_combination_choice():
-    """
-    Prompt a user to choose password character combination which
-    could either be digits, letters, punctuation or combination of
-    either of them.
-    :returns list <class 'list'> of boolean [True, True, False]
-        0 item ---> digits
-        1 item ---> letters
-        2 item ---> punctuation
-    """
-
     # retrieve a user's password character combination choice
     want_digits = input("Want digits ? (True or False) : ")
     want_letters = input("Want letters ? (True or False): ")
@@ -77,18 +49,6 @@ def password_combination_choice():
 
 
 def fetch_string_constant(choices):
-    """
-    Returns a string constant based on users choice_list.
-    string constant can either be digits, letters, punctuation or
-    combination of them.
-    :returns choice_list --> list <class 'list'> of boolean
-        0 item ---> digits
-            True to add digits to constant False otherwise
-        1 item ---> letters
-            True to add letters to constant False otherwise
-        2 item ---> punctuation
-            True to add punctuation to constant False otherwise
-    """
     string_constant = ''
 
     string_constant += NUMBERS if choices[0] else ''
